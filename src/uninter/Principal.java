e452dd4de77b5ad08f047ae5a971acaafbfed7b9
Gabriel <gabriel.fer.faria@gmail.com>
2023-07-16 12:47:22 -0400

    first commit

---
+++ src/uninter/Principal.java
@@ -0,0 +1,13 @@
+package uninter;
+
+
+public class Principal {
+	
+	public static void main(String[]args) {
+		
+			Menu menu= new Menu();
+			menu.menuOpcoes();
+			/*chamada do método menuOpcoes() onde todo o código principal está rodando
+			optei por fazer assim para ter mais liberadade no desenvolvimento*/
+	}
+}
